befdf6b7db39d7868fefee12a8bcc219987fc78f
Naman Gupta <namangupta@Namans-MacBook-Air.local>
2020-07-29 08:17:46 +0530

    google-credentials added

--- hello-world/main.py
+++ hello-world/main.py
@@ -1,6 +1,20 @@
 def hello_world(request):
+    if request.method == 'OPTIONS':
+        headers = {
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Methods': 'POST',
+            'Access-Control-Allow-Headers': 'Content-Type',
+            'Access-Control-Max-Age': '3600'
+        }
+        return '', 204, headers
+
+    headers = {
+        'Access-Control-Allow-Origin': '*'
+    }
+
     request_args = request.args
     request_json = request.get_json(silent=True)
+
     if request_args and 'name' in request_args and request_args and 'last_name' in request_args:
         name = request_args['name']
         last_name = request_args['last_name']
@@ -11,4 +25,4 @@ def hello_world(request):
         name = 'Bruce'
         last_name = 'Wayne'
 
-    return f'Hello {name} {last_name}'
+    return f'Hello {name} {last_name}', 200, headers
